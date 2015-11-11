9c992ab43ca41c4388fe702b388d4185ed9d840a
Joe Martella <martellaj@live.com>
2015-11-11 09:20:21 -0800

    Changed branding in licenses.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,7 +42,7 @@
 
 // *********************************************************
 //
-// O365-Angular-Unified-API-Connect, https://github.com/OfficeDev/O365-Angular-Unified-API-Connect
+// O365-Angular-Microsoft-Graph-Connect, https://github.com/OfficeDev/O365-Angular-Microsoft-Graph-Connect
 //
 // Copyright (c) Microsoft Corporation
 // All rights reserved.
